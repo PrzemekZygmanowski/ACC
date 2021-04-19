0aab502219649362968fd39db050cf767f4f6a46
Zygmanowski, Przemyslaw <przemyslaw.zygmanowski@fujitsu.com>
2021-04-19 22:57:29 +0200

    start implementing redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
 import * as React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
 import './styles.scss';
 import { Header, NotFound, NewProject } from './pages';
+import { Provider } from 'react-redux';
+import store from './store';
 
 export default function App() {
   return (
-    <Router>
-      <Switch>
-        {' '}
-        <Route path='/signUp' component={NotFound} />
-        <Route path='/logIn' component={NotFound} />
-        <Route path='/newProject' component={NewProject} />
-        <Route path='/' component={Header} />
-      </Switch>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Switch>
+          {' '}
+          <Route path='/signUp' component={NotFound} />
+          <Route path='/logIn' component={NotFound} />
+          <Route path='/newProject' component={NewProject} />
+          <Route path='/' component={Header} />
+        </Switch>
+      </Router>
+    </Provider>
   );
 }
